3b0f5dd9fe7c322cb5d726a203bdbd88db57e934
Soujash Mandal <soujashmandal@gmail.com>
2026-03-06 10:22:52 +0530

    Update localgpt.rb

--- Formula/localgpt.rb
+++ Formula/localgpt.rb
@@ -2,7 +2,7 @@ class Localgpt < Formula
     desc "Run LocalGPT using Docker and Ollama"
     homepage "https://github.com/soujashmandal/localgpt"
     url "https://github.com/soujashmandal/localgpt/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "PUT_SHA256_HERE"
+    sha256 "379c68035e668a22e7e11b33cb9874da432ef82abc0810f1790fb6da1d03f416"
     license "MIT"
   
     depends_on "docker"
